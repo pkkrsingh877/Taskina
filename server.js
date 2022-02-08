1f0e5fe7285a076a5243052593a42d5b224e1ef4
Prabhat Kumar <pkkrsingh877@gmail.com>
2022-02-08 14:03:56 +0530

    following changes
    1. Finished up ejs and ejs-mate setup \n2. Finished up MongoDB Setup \n3. Setup method override for future use \n4. Setup Morgan for logging stuff on console

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const morgan = require('morgan');
+const ejsMate = require('ejs-mate');
+const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
-app.get('/', (req, res) => {
-    res.send('Working');
-});
+// middlewares
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(morgan('tiny'));
+
+// ejs and ejs-mate setup
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// setting up mongodb 
+mongoose.connect('mongodb://localhost:27017/taskina' || process.env.MONGO_URI)
+    .then(() => {
+        console.log("Connection Successful!");
+    })
+    .catch((err) => {
+        console.log("Connection Failed!");
+        console.log(err);
+    });
 
 app.listen(port, () => {
     console.log("App is listening on port", port);
